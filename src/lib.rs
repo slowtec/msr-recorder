aa152d33171dfad4040c14053b097405edb46dc4
Markus Kohlhase <mail@markus-kohlhase.de>
2018-06-27 19:38:17 +0200

    initial draft

---
+++ src/lib.rs
@@ -0,0 +1,231 @@
+#[macro_use]
+extern crate log;
+extern crate chrono;
+extern crate csv;
+extern crate msr;
+
+use chrono::prelude::*;
+use csv::Writer;
+use msr::*;
+use std::{
+    collections::HashMap, fs, fs::OpenOptions, io::{self, Result}, path::{Path, PathBuf},
+};
+
+/// A simple CSV recoder implementation.
+pub struct CsvRecorder {
+    created_header: bool,
+    file_name: PathBuf,
+    key_list: Vec<String>,
+    states: Vec<(DateTime<Utc>, HashMap<String, Value>)>,
+}
+
+/// Get a list of values names that can be recorded.
+trait RecKeys {
+    fn rec_keys(&self) -> Vec<String>;
+}
+
+/// Get a recordable map of values.
+trait RecVals {
+    fn rec_vals(&self) -> HashMap<String, Value>;
+}
+
+impl RecKeys for Loop {
+    fn rec_keys(&self) -> Vec<String> {
+        let mut keys = vec![format!("setpoint.{}", self.id)];
+        let inputs: Vec<_> = self.inputs.iter().map(|id| format!("in.{}", id)).collect();
+        let outputs: Vec<_> = self.outputs
+            .iter()
+            .map(|id| format!("out.{}", id))
+            .collect();
+        use ControllerConfig::*;
+        let (c_type, c_keys) = match self.controller {
+            Pid(_) => ("pid", vec!["target", "prev_value", "p", "i", "d"]),
+            BangBang(_) => ("bb", vec!["threshold", "current"]),
+        };
+        let cntrl: Vec<_> = c_keys
+            .iter()
+            .map(|key| format!("controller.{}.{}.{}", self.id, c_type, key))
+            .collect();
+        keys.extend_from_slice(&inputs);
+        keys.extend_from_slice(&outputs);
+        keys.extend_from_slice(&cntrl);
+        keys
+    }
+}
+
+impl RecKeys for SyncRuntime {
+    fn rec_keys(&self) -> Vec<String> {
+        let mut keys = vec![];
+        for (_, interval_loops) in self.loops.iter() {
+            for l in interval_loops.iter() {
+                keys.extend_from_slice(&l.rec_keys());
+            }
+        }
+        keys.sort();
+        keys.dedup();
+        keys
+    }
+}
+
+impl RecVals for IoState {
+    fn rec_vals(&self) -> HashMap<String, Value> {
+        let mut map = HashMap::new();
+        let inputs: HashMap<String, Value> = self.inputs
+            .iter()
+            .map(|(id, v)| (format!("in.{}", id), v.clone()))
+            .collect();
+        let outputs: HashMap<String, Value> = self.outputs
+            .iter()
+            .map(|(id, v)| (format!("out.{}", id), v.clone()))
+            .collect();
+        for (k, v) in inputs {
+            map.insert(k, v);
+        }
+        for (k, v) in outputs {
+            map.insert(k, v);
+        }
+        map
+    }
+}
+
+impl RecVals for msr::SyncRuntimeState {
+    fn rec_vals(&self) -> HashMap<String, Value> {
+        let mut map = HashMap::new();
+        for (id, c) in self.controllers.iter() {
+            use ControllerState::*;
+            match c {
+                Pid(s) => {
+                    map.insert(
+                        format!("controller.{}.pid.target", id),
+                        Value::from(s.target),
+                    );
+                    if let Some(v) = s.prev_value {
+                        map.insert(format!("controller.{}.pid.prev_value", id), Value::from(v));
+                    }
+                    map.insert(format!("controller.{}.pid.p", id), Value::from(s.p));
+                    map.insert(format!("controller.{}.pid.i", id), Value::from(s.i));
+                    map.insert(format!("controller.{}.pid.d", id), Value::from(s.d));
+                }
+                BangBang(s) => {
+                    map.insert(
+                        format!("controller.{}.bb.threshold", id),
+                        Value::from(s.threshold),
+                    );
+                    map.insert(
+                        format!("controller.{}.bb.current", id),
+                        Value::from(s.current),
+                    );
+                }
+            }
+        }
+        for (id, state) in self.rules.iter() {
+            map.insert(format!("rule.{}", id), Value::from(*state));
+        }
+        map
+    }
+}
+
+impl RecVals for SyncSystemState {
+    fn rec_vals(&self) -> HashMap<String, Value> {
+        let mut map = HashMap::new();
+        for (k, v) in self.io.rec_vals() {
+            map.insert(k, v);
+        }
+        for (k, v) in self.runtime.rec_vals() {
+            map.insert(k, v);
+        }
+        for (k, v) in self.setpoints.iter() {
+            map.insert(format!("setpoint.{}", k), v.clone());
+        }
+        map
+    }
+}
+
+impl CsvRecorder {
+    /// Create a new recorder instance.
+    pub fn new(dir: &Path, runtime: &SyncRuntime) -> Result<Self> {
+        let key_list = runtime.rec_keys();
+        let file_name = create_file_name(dir);
+        if let Err(err) = fs::create_dir_all(dir) {
+            if err.kind() != io::ErrorKind::AlreadyExists {
+                return Err(err);
+            }
+        }
+        Ok(CsvRecorder {
+            created_header: false,
+            file_name,
+            key_list,
+            states: vec![],
+        })
+    }
+
+    /// Add a map of values to the internal buffer.
+    pub fn record(&mut self, time: DateTime<Utc>, values: HashMap<String, Value>) {
+        self.states.push((time, values));
+    }
+
+    /// Add a state to the internal buffer.
+    pub fn record_state(&mut self, time: DateTime<Utc>, state: &SyncSystemState) {
+        self.states.push((time, state.rec_vals()));
+    }
+
+    /// Write buffred values to disk.
+    pub fn persist(&mut self) -> Result<()> {
+        if self.states.is_empty() {
+            warn!("no states to persist");
+            return Ok(());
+        }
+
+        let file = OpenOptions::new()
+            .create(true)
+            .write(true)
+            .append(true)
+            .open(&self.file_name)?;
+
+        let mut writer = Writer::from_writer(file);
+
+        self.states.sort_by(|(t1, _), (t2, _)| t1.cmp(&t2));
+
+        for (time, state) in self.states.iter() {
+            if !self.created_header {
+                let mut rec = vec!["timestamp".to_string()];
+                rec.extend_from_slice(self.key_list.as_slice());
+                writer.write_record(rec)?;
+                self.created_header = true;
+            }
+
+            let vals: Vec<_> = self.key_list
+                .iter()
+                .map(|key| match state.get(key) {
+                    Some(v) => {
+                        use Value::*;
+                        match v {
+                            Decimal(d) => d.to_string(),
+                            Integer(i) => i.to_string(),
+                            Bit(b) => b.to_string(),
+                            Text(t) => t.clone(),
+                            Bin(_) => {
+                                warn!("The binary data of '{}' will not be recorded", key);
+                                "".to_string()
+                            }
+                        }
+                    }
+                    None => "".to_string(),
+                })
+                .collect();
+
+            let mut rec = vec![time.timestamp_millis().to_string()];
+            rec.extend_from_slice(vals.as_slice());
+            writer.write_record(rec)?;
+        }
+        writer.flush()?;
+        self.states = vec![];
+        Ok(())
+    }
+}
+
+fn create_file_name(dir: &Path) -> PathBuf {
+    let now = Utc::now();
+    let file_name = now.format("%Y-%m-%d_%H-%M-%S").to_string();
+    dir.join(file_name).with_extension("csv")
+}
